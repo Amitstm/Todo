0a8bb3f4f5d7449663b6d9a08dcb36ba6346cfb5
Amitstm <amitkumarmahto8=76@gmail.com>
2021-04-30 16:06:09 +0530

    server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
+// Serve only the static files form the dist directory
 app.use(express.static("./dist/todo"));
 
 app.get("/*", (req, res) => res.sendFile("index.html", { root: "dist/todo/" }));
