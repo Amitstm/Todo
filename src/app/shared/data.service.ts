3b54c917486d1c7bb9bf826fd4023bd7333a353d
Amitstm <amitkumarmahto8=76@gmail.com>
2021-04-30 15:39:19 +0530

    updates

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -7,8 +7,6 @@ import { Todo } from './todo.model';
 export class DataService {
 
   todos: Todo[] = [
-    // new Todo('this is a test!', false),
-    // new Todo('Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptates dolorem alias dolores deserunt, qui, amet odio facilis tempora unde sequi numquam explicabo nihil iste labore beatae ea rerum expedita.', true)
   ]
   
   constructor() { }
